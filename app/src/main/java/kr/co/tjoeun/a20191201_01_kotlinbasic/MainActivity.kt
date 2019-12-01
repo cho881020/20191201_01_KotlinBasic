f09abf085e4b278b132fd8ca3cb536f9adaa93ee
조경진 <kj_cho@idealidea.co.kr>
2019-12-01 10:25:19 +0900

    초기 화면 그리기 + 버튼 클릭 이벤트 감지

--- app/src/main/java/kr/co/tjoeun/a20191201_01_kotlinbasic/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/a20191201_01_kotlinbasic/MainActivity.kt
@@ -2,11 +2,36 @@ package kr.co.tjoeun.a20191201_01_kotlinbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        Ctrl + / 주석
+//        버튼 (okBtn)이 눌리면
+//        EditText (inputEdt)에 적힌 값을 받아서
+//        TextView (resultTxt)의 내용으로 반영.
+
+        okBtn.setOnClickListener {
+//            Ctrl + K 커밋
+        }
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
